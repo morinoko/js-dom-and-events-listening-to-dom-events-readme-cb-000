c2475ee8e71d5bad8aaf1b69f4cf35984ade783c
Felice Forby <feliceforby@gmail.com>
2019-02-17 05:15:33 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -2,12 +2,19 @@
 // you can also load the code snippets in using your browser's console
 const main = document.getElementById('main');
 
-main.addEventListener('click', function(event) {
-  alert('I was clicked');
-});
-
 const input = document.querySelector('input');
 
 input.addEventListener('keydown', function(e) {
   console.log(e.which);
+});
+
+const input = document.querySelector('input');
+ 
+input.addEventListener('keydown', function(e) {
+  if (e.which === 71) {
+    console.log('default prevented');
+    return e.preventDefault();
+  } else {
+    console.log('Not a "g"');
+  }
 });
